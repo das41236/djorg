8a95157f6f126e2d7415948c7590acf612e0fc33
unknown <davsoudry@gmail.com>
2018-05-31 00:16:41 -0500

    fixed api bug

--- notes/api.py
+++ notes/api.py
@@ -3,11 +3,12 @@ from .models import Note
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
 
-    # def create(self, validated_data):
-#         user = self.context['request'].user
+    def create(self, validated_data):
+        # import pdb; pdb.set_trace()
+        user = self.context['request'].user
 
-#         note = Note.objects.create(user=user, **validated_data)
-#         return note
+        note = Note.objects.create(user=user, **validated_data)
+        return note
 
     class Meta:
         model = Note
